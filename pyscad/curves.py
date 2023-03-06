620085e5f7a7b4f77fd90b9011f117e40f79afbb
Jef Wagner <jefwagner@gmail.com>
2023-03-06 08:47:11 -0600

    moved nurbs to its own source files

--- pyscad/curves.py
+++ pyscad/curves.py
@@ -4,9 +4,7 @@ import numpy as np
 import numpy.typing as npt
 import scipy.integrate as integrate
 
-from .flint import v_flint
-from .cpoint import CPoint, cp_mag, cp_unit, cp_vectorize
-from .kvec import KnotVector
+from .cpoint import CPoint, cp_mag, cp_unit
 
 class SpaceCurve:
     """An abstract base class for D-dimensional parametric curve
@@ -77,79 +75,3 @@ class SpaceCurve:
         cmag = cmag.reshape(sh)
         tmag = cp_mag(t)
         return cmag/(tmag*tmag*tmag)
-
-
-class NurbsCurve(SpaceCurve):
-    """Non-uniform Rational Basis Splines"""
-
-    binom = np.array([
-        [1,0,0,0,0],
-        [1,1,0,0,0],
-        [1,2,1,0,0],
-        [1,3,3,1,0],
-        [1,4,6,4,1],
-    ])
-
-    def __init__(self, 
-                 c: Sequence[CPoint], 
-                 w: Sequence[float], 
-                 p: int, 
-                 t: Sequence[float]):
-        """Create a new NURBS curve object
-        @param c The control points
-        @param w The weights
-        @param p Degree of the b-spline basis functions
-        @param t the knot-vector
-        """
-        if len(c) != len(w):
-            raise ValueError('The control points and weights must have the same length')
-        if len(t) != len(c) + p + 1:
-            raise ValueError('Knot vector wrong length')
-        self.c = v_flint(c)
-        self.w = np.array(w, dtype=np.float64)
-        self.p = p
-        self.t = KnotVector(t)
-    
-    def __call__(self, x: float) -> CPoint:
-        """Evaluate the Nurbs curve
-        @param x Parametric value
-        @return Point along the spline
-        """
-        wc = (self.c.T*self.w).T
-        c = self.t.deboor(self.p, wc, x)
-        w = self.t.deboor(self.p, self.w, x)
-        return (c.T/w).T
-
-    def d_list(self, x: float, n: int = 1) -> List[CPoint]:
-        """Evaluate the value and derivatives of the Nurbs curvs
-        @param x Parametric value
-        @param n The order of the derivative
-        @return A list of the value and higher order derivatives of the spline curve
-        """
-        c, w, s = [], [], []
-        wc = (c.T*w).T
-        _w = self.w.copy()
-        c.append(self.t.deboor(self.p, wc, x))
-        w.append(self.t.deboor(self.p, _w, x))
-        s.append((c[0].T/w[0]).T)
-        for i in range(n):
-            wc = self.t.d_points(self.p-i, wc, 1)
-            _w = self.t.d_points(self.p-i, _w, 1)
-            c.append(self.t.deboor(self.p-i-1, wc, x))
-            w.append(self.t.deboor(self.p-i-1, _w, x))
-            # calc the next derivative
-            res = c[-1]
-            for k in range(1,i+2):
-                res -= self._binom[i+1,k]*((s[i+1-k].T*w[k]).T)
-            s.append((res.T/w[0]).T)
-        return s
-
-    def d(self, x: float, n: int = 1) -> CPoint:
-        """Evaluate the derivative of the b-spline with respect
-        @param x Parametric value
-        @param n The order of the derivative
-        @return The n^th derivative at the point x along the spline
-        """
-        return self.d_list(x, n)[-1]
-        
-
