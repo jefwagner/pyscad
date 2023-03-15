01eaccb92d3b75d9e0314f1360d096f71826b0c0
Jef Wagner <jefwagner@gmail.com>
2023-03-14 21:38:41 -0500

    added license

--- pyscad/__init__.py
+++ pyscad/__init__.py
@@ -1,4 +1,18 @@
-"""@file Exporting the public parts of the module"""
+"""@file Exporting the public parts of the module
+
+This file is part of pyscad.
+
+pyscad is free software: you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation, either version
+3 of the License, or (at your option) any later version.
+
+pyscad is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with Foobar. If
+not, see <https://www.gnu.org/licenses/>.
+"""
 
 # Export floating point interval objects and the v_flint helper function
 from .flint import flint, v_flint
