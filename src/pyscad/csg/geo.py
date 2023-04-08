885b13a276053c7a413a9728427947708256cce8
Jef Wagner <jefwagner@gmail.com>
2023-04-07 20:58:00 -0500

    First draft of transformation classes

--- src/pyscad/csg/geo.py
+++ src/pyscad/csg/geo.py
@@ -19,9 +19,11 @@ def is_vec(v: Any, length: Optional[int] = None) -> bool:
     """Test if input is a generic vector"""
     try:
         a = (len(v) in [1, 2,3]) if length is None else (len(v) == length)
+        if not a:
+            return False
         if isinstance(v, np.ndarray):
-            return a and v.dtype in (int, float, flint)
-        return a and functools.reduce(lambda a, b: a and b, map(is_num, v))
+            return v.dtype in (int, float, flint)
+        return functools.reduce(lambda a, b: a and b, map(is_num, v))
     except:
         return False
 
@@ -57,12 +59,13 @@ class Transform:
         """Apply a transformation"""
         if isinstance(a, Transform):
             t = Transform(len(a))
-            t.m = self.m.dot(a.m) + self.v.dot(a.m)
+            t.m = self.m.dot(a.m)
             t.v = self.m.dot(a.v) + self.v
             return t
         return self.m.dot(a) + self.v
 
     def __eq__(self, other: 'Transform') -> bool:
+        """Compare if two transformations are equivalent"""
         if not isinstance(other, Transform):
             raise ValueError("Can only compare transforms")
         if len(self.v) != len(other.v):
@@ -74,11 +77,18 @@ class Scale(Transform):
     """Scale"""
 
     def __init__(self, s: Union[Num, Vec]):
+        """Create a new Scale transformations
+
+        @param s If s is a scalar then create a uniform 3D scaling
+        transformation, else if s is a vector then scales the each axis
+        according to the vectors components. The only way to obtain a 2D uniform
+        scaling is to pass in 2D vector with equal components. 
+        """
         if is_num(s):
             self.m = np.array([[s,0,0],[0,s,0],[0,0,s]], dtype=flint)
             self.v = np.zeros(3, dtype=flint)
         elif is_vec(s):
-            self.m = np.diag(s, dtype=flint)
+            self.m = np.diag(s).astype(flint)
             self.v = np.zeros(len(s), dtype=flint)
         else:
             raise ValueError("Scale must be set with a scalar or vector")
@@ -87,11 +97,36 @@ class Scale(Transform):
 class Translate(Transform):
     """Translate"""
 
-    def __init__(self, dx):
-        ...
+    def __init__(self, dx: Vec):
+        if is_vec(dx):
+            self.m = np.eye(len(dx), dtype=flint)
+            self.v = np.array(dx, dtype=flint)
+        else:
+            raise ValueError("Translate must be set with a vector")
+
 
 class Rotate(Transform):
     """Rotation"""
 
-    def __init__(self, axis, angle):
-        ...
+    def __init__(self, angle: Num, axis: Optional[Vec] = None):
+        if not is_num(angle):
+            raise ValueError("Angle must be a number")
+        th = flint(angle)
+        if axis is None:
+            self.m = np.array([
+                [np.cos(th), -np.sin(th)],
+                [np.sin(th), np.cos(th)]
+            ], dtype=flint)
+            self.v = np.zeros(2, dtype=flint)
+        elif is_vec(axis) and len(axis) == 3:
+            u = np.array(axis, dtype=flint)
+            x, y, z = u/np.sqrt(np.sum(u*u))
+            c, s = np.cos(th), np.sin(th)
+            self.m = np.array([
+                [c+x*x*(1-c), x*y*(1-c)-z*s, x*z*(1-c)+y*s],
+                [y*z*(1-c)+z*s, c+y*y*(1-c), y*z*(1-c)-x*s],
+                [z*x*(1-c)-y*s, z*y*(1-c)+x*s, c+z*z*(1-c)]
+            ], dtype=flint)
+            self.v = np.zeros(3, dtype=flint)
+        else:
+            raise ValueError("Axis must be a 3 dimensional vector")
--- tests/test_geo.py
+++ tests/test_geo.py
@@ -90,8 +90,20 @@ class TestTransform:
         assert t is not t2
         assert t == t2
 
-#     def test_apply_transform(self):
-#         assert False
+    def test_apply_transform(self):
+        t = Transform()
+        t.m = np.array([[2,0,0],[0,3,0],[0,0,4]], dtype=flint)
+        t.v = np.ones(3, dtype=flint)
+        ident = Transform()
+        t2 = ident(t)
+        assert isinstance(t2, Transform)
+        assert len(t2) == 3
+        assert t2 is not t
+        assert t2 == t
+        t2 = t(ident)
+        assert t2 is not t
+        assert t2 == t
+
 
 class TestScale:
 
@@ -102,3 +114,61 @@ class TestScale:
         assert len(s) == 3
         assert np.alltrue( s.m == np.array([[2,0,0],[0,2,0],[0,0,2]]) )
         assert np.alltrue( s.v == np.zeros(3) )
+        s = Scale([2,3])
+        assert isinstance(s, Transform)
+        assert isinstance(s, Scale)
+        assert len(s) == 2
+        assert np.alltrue( s.m == np.array([[2,0],[0,3]]) )
+        assert np.alltrue( s.v == np.zeros(2) )
+        s = Scale([2,3,4])
+        assert isinstance(s, Transform)
+        assert isinstance(s, Scale)
+        assert len(s) == 3
+        assert np.alltrue( s.m == np.array([[2,0,0],[0,3,0],[0,0,4]]) )
+        assert np.alltrue( s.v == np.zeros(3) )
+        with pytest.raises(ValueError):
+            s = Scale([2,3,4,5])
+        with pytest.raises(ValueError):
+            s = Scale("foo")
+    
+
+class TestTranslate:
+
+    def test_init(self):
+        t = Translate([1,2])
+        assert isinstance(t, Transform)
+        assert isinstance(t, Translate)
+        assert len(t) == 2
+        assert np.alltrue( t.m == np.eye(2) )
+        assert np.alltrue( t.v == np.array([1,2]) )
+        t = Translate([1,2,3])
+        assert isinstance(t, Transform)
+        assert isinstance(t, Translate)
+        assert len(t) == 3
+        assert np.alltrue( t.m == np.eye(3) )
+        assert np.alltrue( t.v == np.array([1,2,3]) )
+        with pytest.raises(ValueError):
+            t = Translate([2,3,4,5])
+        with pytest.raises(ValueError):
+            t = Translate("foo")
+
+
+class TestRotate:
+
+    def test_init(self):
+        r = Rotate(np.pi/2)
+        assert isinstance(r, Transform)
+        assert isinstance(r, Rotate)
+        assert len(r) == 2
+        assert np.alltrue( r.m == np.array([[0,-1],[1,0]]) )
+        assert np.alltrue( r.v == np.zeros(2) )
+        r = Rotate(np.pi/2, [0,0,1])
+        assert isinstance(r, Transform)
+        assert isinstance(r, Rotate)
+        assert len(r) == 3
+        assert np.alltrue( r.m == np.array([[0,-1,0],[1,0,0],[0,0,1]]) )
+        assert np.alltrue( r.v == np.zeros(3) )
+        with pytest.raises(ValueError):
+            r = Rotate(np.pi/2, [0,0])
+        with pytest.raises(ValueError):
+            r = Rotate("foo")
