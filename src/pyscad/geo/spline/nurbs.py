4956a9c401bbf8b47e0291f2fb7b63497316099f
Jef Wagner <jefwagner@gmail.com>
2023-05-07 21:45:36 -0500

    rewrote nurbs to vectorize over derivative orders

--- src/pyscad/geo/spline/nurbs.py
+++ src/pyscad/geo/spline/nurbs.py
@@ -59,8 +59,11 @@ class NurbsCurve(BSplineCurve):
         @param n The order of the derivative
         @return The value of the derivative curve at the parametric point x
         """
-        out_shape = list(np.shape(x)) + list(np.shape(self.cpts[0]))
+        v_shape = list(np.shape(x)) + list(np.shape(self.cpts[0]))
+        out_shape = list(np.shape(n)) + v_shape
         out_array = np.zeros(out_shape, dtype=flint)
+        # v_array = np.zeros(v_shape, dtype=flint)
+        nmax = np.max(n)
         with np.nditer(np.array(x), flags=['multi_index']) as it:
             for xx in it:
                 c = self.cpts_array[0]
@@ -68,20 +71,22 @@ class NurbsCurve(BSplineCurve):
                 c_list = [self._deboor_1d(c, xx)]
                 w_list = [self._deboor_1d(w, xx)]
                 s_list = [c_list[0]/(w_list[0] if w_list[0] != 0 else 1)]
-                for i in range(1,n+1):
+                for i in range(1,nmax+1):
                     if self.cpts_array[i] is None:
                         self._calc_d_cpts(self.cpts_array, i)
                         self._calc_d_cpts(self.w_array, i)
                     c = self.cpts_array[i]
                     w = self.w_array[i]
-                    # wc = c*w[...,np.newaxis]
                     c_list.append(self._deboor_1d(c, xx, i))
                     w_list.append(self._deboor_1d(w, xx, i))
                     res = c_list[-1]
                     for k in range(1,i+1):
                         res -= _binom[i,k]*(s_list[i-k]*w_list[k])
                     s_list.append(res/(w_list[0] if w_list[0] != 0 else 1))
-                out_array[it.multi_index] = s_list[-1]
+                with np.nditer(np.array(n), flags=['multi_index']) as der_iter:
+                    for nn in der_iter:
+                        idx = tuple(list(der_iter.multi_index) + list(it.multi_index))
+                        out_array[idx] = s_list[nn]
         return out_array
 
 
@@ -122,7 +127,11 @@ class NurbsSurf(BSplineSurf):
         # Holder for output data
         if np.shape(u) != np.shape(v):
             raise ValueError("u and v arguments must be same shape")
-        out_shape = list(np.shape(u)) + list(np.shape(self.cpts[0,0]))
+        if np.shape(nu) != np.shape(nv):
+            raise ValueError("nu and nv derivative orders must be same shape")
+        c_shape = list(np.shape(self.cpts[0,0]))
+        v_shape = list(np.shape(u)) + c_shape
+        out_shape = list(np.shape(nu)) + v_shape
         out_array = np.zeros(out_shape, dtype=flint)
         # Working space
         w_arr_shape = (nu+1, nv+1)
@@ -130,28 +139,15 @@ class NurbsSurf(BSplineSurf):
         c_arr = np.zeros(c_arr_shape, dtype=flint)
         w_arr = np.zeros(w_arr_shape, dtype=flint)
         s_arr = np.zeros(c_arr_shape, dtype=flint)
+        numax = np.max(nu)
+        nvmax = [0 for _ in range(numax+1)]
+        for du, dv in np.nditer([np.array(nu), np.array(nv)]):
+            for i in range(du+1):
+                nvmax[i] = nvmax[i] if nvmax[i] >= dv else dv
         with np.nditer([np.array(u),np.array(v)], flags=['multi_index']) as it:
             for uu, vv in it:
-                c = self.cpts_array[0][0]
-                w = self.w_array[0][0]
-                c_arr[0,0] = self._deboor_2d(c, uu, vv)
-                _w = self._deboor_2d(w, uu, vv)
-                w_arr[0,0] = _w if _w != 0 else flint(1.0)
-                s_arr[0,0] = c_arr[0,0]/w[0,0]
-                for j in range(1, nv+1):
-                    if self.cpts_array[0][j] is None:
-                        self._calc_d_cpts_2d(self.cpts_array, 0, j)
-                        self._calc_d_cpts_2d(self.w_array, 0, j)
-                    c = self.cpts_array[0][j]
-                    w = self.w_array[0][j]
-                    c_arr[0,j] = self._deboor_2d(c, uu, vv, 0, j)
-                    w_arr[0,j] = self._deboor_2d(w, uu, vv, 0, j)
-                    s_arr[0,j] = c_arr[0,j]
-                    for jj in range(1, j+1):
-                        s_arr[0,j] -= _binom[j, jj]*s_arr[0,j-jj]*w_arr[0,jj]
-                    s_arr[0,j] /= w_arr[0,j]
-                for i in range(1, nu-1):
-                    for j in range(nv):
+                for i in range(0, numax+1):
+                    for j in range(nvmax[i]+1):
                         if self.cpts_array[i][j] is None:
                             self._calc_d_cpts_2d(self.cpts_array, uu, vv, i, j)
                             self._calc_d_cpts_2d(self.w_array, uu, vv, i, j)
@@ -159,6 +155,8 @@ class NurbsSurf(BSplineSurf):
                         w = self.w_array[i][j]
                         c_arr[i,j] = self._deboor_2d(c, uu, vv, i, j)
                         w_arr[i,j] = self._deboor_2d(w, uu, vv, i, j)
+                        if i == 0 and j == 0:
+                            w_arr[0,0] = w_arr[0,0] if w_arr[0,0] != 0 else flint(1.0)
                         s_arr[i,j] = c_arr[i,j]
                         for ii in range(1, i+1):
                             for jj in range(j+1):
@@ -166,5 +164,8 @@ class NurbsSurf(BSplineSurf):
                                 term *= s_arr[i-ii,j-jj]*w_arr[ii,jj]
                                 s_arr[i,j] -= term
                         s_arr[i,j] /= w_arr[0,0]
-                out_array[it.multi_index] = s_arr[nu,nv]
+                with np.nditer([np.array(nu), np.array(nv)], flags=['multi_index']) as der_iter:
+                    for du, dv in der_iter:
+                        idx = tuple(list(der_iter.multi_index) + list(it.multi_index))
+                        out_array[idx] = s_arr[du,dv]
         return out_array
