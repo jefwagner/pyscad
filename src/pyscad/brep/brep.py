1b70b1540f5571dda3311a58f1d8903a53cd37ca
Jef Wagner <jefwagner@gmail.com>
2023-06-02 20:06:02 -0500

    need to rethink breps

--- src/pyscad/brep/brep.py
+++ src/pyscad/brep/brep.py
@@ -45,6 +45,8 @@ class BRep:
 
     @classmethod
     def sphere(cls):
+        sp = cls.__init__()
+        # Get the surfaces
         c = np.array([
             [[1,0,0],[1,1,0],[0,1,0]],
             [[1,0,1],[1,1,1],[0,1,1]],
@@ -58,37 +60,86 @@ class BRep:
         rot90 = Transform.from_arrays(m, z)
         m = np.array([[1,0,0],[0,1,0],[0,0,-1]], dtype=flint)
         reflz = Transform.from_arrays(m, z)
+        # first octant: x+, y+, z+
         sppp = NurbsSurf(c, w, 2, 2, t, t)
+        # second octant: x-, y+, z+
         for idx in np.ndindex((3,3)):
             c[idx] = rot90(c[idx])
         snpp = NurbsSurf(c, w, 2, 2, t, t)
+        # third octant: x-, y-, z+
         for idx in np.ndindex((3,3)):
             c[idx] = rot90(c[idx])
         snnp = NurbsSurf(c, w, 2, 2, t, t)
+        # fourth octant: x+, y-, z+
         for idx in np.ndindex((3,3)):
             c[idx] = rot90(c[idx])
         spnp = NurbsSurf(c, w, 2, 2, t, t)
+        # fifth octanct: x+, y+, z-
         for idx in np.ndindex((3,3)):
             c[idx] = rot90(c[idx])
             c[idx] = reflz(c[idx])
         sppn = NurbsSurf(c, w, 2, 2, t, t)
+        # sixth octant: x-, y+, z-
         for idx in np.ndindex((3,3)):
             c[idx] = rot90(c[idx])
         snpn = NurbsSurf(c, w, 2, 2, t, t)
+        # seventh octant: x-, y-, z-
         for idx in np.ndindex((3,3)):
             c[idx] = rot90(c[idx])
         snnn = NurbsSurf(c, w, 2, 2, t, t)
+        # eighth octant: x+, y-, z-
         for idx in np.ndindex((3,3)):
             c[idx] = rot90(c[idx])
         spnn = NurbsSurf(c, w, 2, 2, t, t)
-        self.surfs.extend([
-            sppp,
-            snpp,
-            snnp,
-            spnp,
-            sppn,
-            snpn,
-            snnn,
-            spnn
+        # set the surfaces
+        sp.surfs.extend([
+            sppp, snpp, snnp, spnp,
+            sppn, snpn, snnn, spnn
         ])
-
+        # curves
+        cp = np.array([[1,0,0],[1,0,1],[0,0,1]], dtype=flint)
+        cz = np.array([[1,0,0],[1,1,0],[0,1,0]], dtype=flint)
+        cn = np.array([[1,0,0],[1,0,-1],[0,0,-1]], dtype=flint)
+        w = [1,a,1]
+        # x+, y+ quadrant
+        sp.curves.extend([
+            NurbsCurve(cp, w, 2, t),
+            NurbsCurve(cz, w, 2, t),
+            NurbsCurve(cn, w, 2, t),
+        ])
+        # x-, y+ quadrant
+        for idx in np.ndindex((3,)):
+            cp[idx] = rot90(cp[idx])
+            cz[idx] = rot90(cz[idx])
+            cn[idx] = rot90(cn[idx])
+        sp.curves.extend([
+            NurbsCurve(cp, w, 2, t),
+            NurbsCurve(cz, w, 2, t),
+            NurbsCurve(cn, w, 2, t),
+        ])
+        # x-, y- quadrant
+        for idx in np.ndindex((3,)):
+            cp[idx] = rot90(cp[idx])
+            cz[idx] = rot90(cz[idx])
+            cn[idx] = rot90(cn[idx])
+        sp.curves.extend([
+            NurbsCurve(cp, w, 2, t),
+            NurbsCurve(cz, w, 2, t),
+            NurbsCurve(cn, w, 2, t),
+        ])
+        # x+, y- quadrant
+        for idx in np.ndindex((3,)):
+            cp[idx] = rot90(cp[idx])
+            cz[idx] = rot90(cz[idx])
+            cn[idx] = rot90(cn[idx])
+        sp.curves.extend([
+            NurbsCurve(cp, w, 2, t),
+            NurbsCurve(cz, w, 2, t),
+            NurbsCurve(cn, w, 2, t),
+        ])
+        # verticies
+        sp.verts = [np.array(x, dtype=flint) for x in 
+            [[0,0,1],[1,0,0],[0,1,0],[-1,0,0],[0,-1,0],[0,0,-1]]
+        ]
+        #
+        sp.edges.append(Edge()
